180dd79a97d4f5c7b41c5c7806282dad08a03a50
Tomasz Kot <tomaszkot.mail@gmail.com>
2018-07-12 16:08:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tomcat/remindmeapp/RemindersActive.java
@@ -0,0 +1,30 @@
+package com.example.tomcat.remindmeapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment with Active Reminders - Reminder List
+ */
+
+public class RemindersActive extends Fragment{
+
+    public RemindersActive(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.reminders_fragment, container, false);
+    }
+}
